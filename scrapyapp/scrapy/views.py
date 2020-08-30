96b1764579796ef3953813c3748875d3f57e49f1
sheep333 <yukiko.elike1.51@gmail.com>
2020-08-30 10:01:05 +0900

    add scrapy view

--- scrapyapp/scrapy/views.py
+++ scrapyapp/scrapy/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
+from .scraping import Register
+
+
 # Create your views here.
+def scrapy_start(request):
+    url = 'https;//xxx/xxx.jp'
+    register = Register()
+    register.start_login(url, request.user.pk)
+    register.start_regist(url)
